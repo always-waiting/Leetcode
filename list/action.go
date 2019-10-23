4c41a5a109e82659b419d3cdb9f8401279a2b0d1
mixichao <mixichao@jd.com>
2019-10-23 11:45:01 +0800

    [Iter] 链表合并添加输入值检查

--- list/action.go
+++ list/action.go
@@ -1,6 +1,15 @@
 package list
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 != nil {
+		return l2
+	}
+	if l1 != nil && l2 == nil {
+		return l1
+	}
+	if l1 == nil && l2 == nil {
+		return nil
+	}
 	var ret *ListNode
 	if l1.Val <= l2.Val {
 		ret = &ListNode{
