fa99e16067b567985fd1d087d5f68427abc2f437
mixichao <mixichao@jd.com>
2019-10-31 14:07:43 +0800

    [Iter] 对链表进行插入排序
    习题地址: https://leetcode-cn.com/problems/insertion-sort-list/

--- list/action2.go
+++ list/action2.go
@@ -156,3 +156,53 @@ func oddEvenList(head *ListNode) *ListNode {
 	pre.Next = odd
 	return dummy.Next
 }
+
+/*
+对链表进行插入排序。
+插入排序算法：
+1. 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+2. 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+3. 重复直到所有输入数据插入完为止。
+
+示例 1：
+输入: 4->2->1->3
+输出: 1->2->3->4
+
+示例 2：
+输入: -1->5->3->4->0
+输出: -1->0->3->4->5
+*/
+func insertionSortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	pre, cur := head, head.Next
+	for {
+		if cur == nil {
+			break
+		}
+		if cur.Val < pre.Val {
+			insert := insert
+			pre.Next, cur = cur.Next, cur.Next
+			if head.Val > insert.Val {
+				insert.Next, head = head, insert
+			} else {
+				preloop, loop := head, head.Next
+				for {
+					if loop == cur {
+						break
+					}
+					if insert.Val < loop.Val {
+						insert.Next = loop
+						preloop.Next = insert
+						break
+					}
+					preloop, loop = loop, loop.Next
+				}
+			}
+		} else {
+			pre.Next, pre, cur = cur, cur, cur.Next
+		}
+	}
+	return head
+}
