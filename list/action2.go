d10364256c79d05c328b7d9066058210be121197
mixichao <mixichao@jd.com>
2019-10-31 13:08:45 +0800

    [Iter] 分割链表
    习题地址: https://leetcode-cn.com/problems/partition-list/
    陷阱:
    1. 在判断退出条件时，需要对后一个链表的最后一个元素进行tmp.Next = nil操作
    2. b.Next, b = a, b.Next,这种赋值最终b不是指向a的

--- list/action2.go
+++ list/action2.go
@@ -4,11 +4,10 @@ package list
 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
 
 示例 1:
-
 输入: 4->2->1->3
 输出: 1->2->3->4
-示例 2:
 
+示例 2:
 输入: -1->5->3->4->0
 输出: -1->0->3->4->5
 */
@@ -79,4 +78,37 @@ func sortList(head *ListNode) *ListNode {
 		intV = 2 * intV
 	}
 	return res.Next
+}
+
+/*
+给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+你应当保留两个分区中每个节点的初始相对位置。
+
+示例:
+输入: head = 1->4->3->2->5->2, x = 3
+输出: 1->2->2->4->3->5
+*/
+func partition(head *ListNode, x int) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre, cur := dummy, dummy.Next
+	var large, tmp *ListNode
+	for {
+		if cur == nil {
+			if tmp != nil {
+				tmp.Next = nil
+			}
+			break
+		}
+		if cur.Val < x {
+			pre.Next, pre, cur = cur, cur, cur.Next
+		} else {
+			if large == nil {
+				large, tmp, cur = cur, cur, cur.Next
+			} else {
+				tmp.Next, tmp, cur = cur, cur, cur.Next
+			}
+		}
+	}
+	pre.Next = large
+	return dummy.Next
 }
