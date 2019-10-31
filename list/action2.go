bb58f617ee558650af7fe30eee1f889290b387ae
mixichao <mixichao@jd.com>
2019-10-31 13:18:24 +0800

    [Iter] 奇偶链表
    习题地址: https://leetcode-cn.com/problems/odd-even-linked-list/
    
    注释:
    1. 与[分割链表](https://leetcode-cn.com/problems/partition-list/)一个解题思路

--- list/action2.go
+++ list/action2.go
@@ -112,3 +112,47 @@ func partition(head *ListNode, x int) *ListNode {
 	pre.Next = large
 	return dummy.Next
 }
+
+/*
+给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+
+示例 1:
+输入: 1->2->3->4->5->NULL
+输出: 1->3->5->2->4->NULL
+
+示例 2:
+输入: 2->1->3->5->6->4->7->NULL
+输出: 2->3->6->7->1->5->4->NULL
+
+说明:
+应当保持奇数节点和偶数节点的相对顺序。
+链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+*/
+func oddEvenList(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	step := 0
+	pre, cur := dummy, dummy.Next
+	var odd, tmp *ListNode
+	for {
+		if cur == nil {
+			if tmp != nil {
+				tmp.Next = nil
+			}
+			break
+		}
+		if step%2 == 0 {
+			pre.Next, pre, cur = cur, cur, cur.Next
+		} else {
+			if odd == nil {
+				odd, tmp, cur = cur, cur, cur.Next
+			} else {
+				tmp.Next, tmp, cur = cur, cur, cur.Next
+			}
+
+		}
+		step++
+	}
+	pre.Next = odd
+	return dummy.Next
+}
