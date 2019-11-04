4e4a7d620ebb693922bdc825057cc27eb06d739c
mixichao <mixichao@jd.com>
2019-11-04 19:54:49 +0800

    [feat] 有序链表转换二叉搜索树
    习题地址: https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/

--- list/action4.go
+++ list/action4.go
@@ -54,3 +54,50 @@ func reorderList(head *ListNode) {
 	}
 	cur.Next = nil
 }
+
+/*
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+给定的有序链表： [-10, -3, 0, 5, 9],
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+
+func sortedListToBST(head *ListNode) *TreeNode {
+	if head == nil {
+		return nil
+	}
+	if head.Next == nil {
+		return &TreeNode{Val: head.Val}
+	}
+	return createTreeNodeWithMiddleList(head, nil)
+}
+
+func createTreeNodeWithMiddleList(head *ListNode, end *ListNode) *TreeNode {
+	if head == nil {
+		return nil
+	}
+	if head == end {
+		return &TreeNode{Val: head.Val}
+	}
+	fast := head
+	slow := head
+	for {
+		if fast == end || fast.Next == end {
+			break
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	tree_node := &TreeNode{Val: slow.Val}
+	tree_node.Left = createTreeNodeWithMiddleList(head, slow)
+	tree_node.Right = createTreeNodeWithMiddleList(slow.Next, nil)
+	return tree_node
+}
