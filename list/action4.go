70954fba16a80f66879dcc28e785a30ac5b7183b
mixichao <mixichao@jd.com>
2019-11-05 20:59:33 +0800

    [feat] 从链表中删去总和值为零的连续节点
    习题地址: https://leetcode-cn.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/

--- list/action4.go
+++ list/action4.go
@@ -198,3 +198,63 @@ func splitListToParts(root *ListNode, k int) []*ListNode {
 	return ret
 
 }
+
+/*
+给你一个链表的头节点 head，请你编写代码，反复删去链表中由 总和 值为 0 的连续节点组成的序列，直到不存在这样的序列为止。
+删除完毕后，请你返回最终结果链表的头节点。
+
+你可以返回任何满足题目要求的答案。
+（注意，下面示例中的所有序列，都是对 ListNode 对象序列化的表示。）
+
+示例 1：
+输入：head = [1,2,-3,3,1]
+输出：[3,1]
+提示：答案 [1,2,1] 也是正确的。
+
+示例 2：
+输入：head = [1,2,3,-3,4]
+输出：[1,2,4]
+
+示例 3：
+输入：head = [1,2,3,-3,-2]
+输出：[1]
+
+提示：
+给你的链表中可能有 1 到 1000 个节点。
+对于链表中的每个节点，节点的值：-1000 <= node.val <= 1000
+*/
+func removeZeroSumSublists(head *ListNode) *ListNode {
+	var step, total int
+	cur := head
+	for cur == nil {
+		total++
+		cur = cur.Next
+	}
+	length := 2
+	for length <= total {
+		list := head
+		var pre *ListNode
+		for list != nil {
+			sum := 0
+			step := 0
+			cur := list
+			for step == length {
+				sum = cur.Val + sum
+				cur = cur.Next
+				step++
+			}
+			if sum == 0 {
+				if pre != nil {
+					pre.Next = cur
+				} else {
+					head = cur
+				}
+				list = cur
+			} else {
+				pre, list = list, list.Next
+			}
+		}
+		length++
+	}
+	return head
+}
