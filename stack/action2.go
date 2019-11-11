e0bd6443ac2ddbd77514c9646f8b3294c18eeeb7
mixichao <mixichao@jd.com>
2019-11-11 13:54:19 +0800

    [feat] 比较含退格的字符串
    习题地址: https://leetcode-cn.com/problems/backspace-string-compare/

--- stack/action2.go
+++ stack/action2.go
@@ -41,3 +41,59 @@ func removeDuplicates(S string) string {
 	}
 	return string(ret)
 }
+
+/*
+给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
+
+示例 1：
+输入：S = "ab#c", T = "ad#c"
+输出：true
+解释：S 和 T 都会变成 “ac”。
+
+示例 2：
+输入：S = "ab##", T = "c#d#"
+输出：true
+解释：S 和 T 都会变成 “”。
+
+示例 3：
+输入：S = "a##c", T = "#a#c"
+输出：true
+解释：S 和 T 都会变成 “c”。
+
+示例 4：
+输入：S = "a#c", T = "b"
+输出：false
+解释：S 会变成 “c”，但 T 仍然是 “b”。
+
+提示：
+1 <= S.length <= 200
+1 <= T.length <= 200
+S 和 T 只含有小写字母以及字符 '#'。
+*/
+func backspaceCompare(S string, T string) bool {
+	stack1 := Stack{}
+	stack2 := Stack{}
+	for _, val := range S {
+		if string(val) == "#" {
+			stack1.Pop()
+		} else {
+			stack1.Push(val)
+		}
+	}
+	for _, val := range T {
+		if string(val) == "#" {
+			stack2.Pop()
+		} else {
+			stack2.Push(val)
+		}
+	}
+	for !stack1.IsEmpty() && !stack2.IsEmpty() {
+		if stack1.Pop().Rune() != stack2.Pop().Rune() {
+			return false
+		}
+	}
+	if !stack1.IsEmpty() || !stack2.IsEmpty() {
+		return false
+	}
+	return true
+}
