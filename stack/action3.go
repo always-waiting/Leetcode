0b9db6f2f02551857e0fd8e095b8173a5335404c
mixichao <mixichao@jd.com>
2019-11-11 19:27:52 +0800

    [feat] 每日温度
    习题地址: https://leetcode-cn.com/problems/daily-temperatures/

--- stack/action3.go
+++ stack/action3.go
@@ -113,3 +113,29 @@ func preorderTraversal1(root *TreeNode) []int {
 	}
 	return ret
 }
+
+/*
+根据每日 气温 列表，请重新生成一个列表，对应位置的输入是你需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+
+例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+
+提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+*/
+func dailyTemperatures(T []int) []int {
+	stack := Stack{}
+	ret := make([]int{}, len(T))
+	var slice int
+	for i := len(T) - 1; i >= 0; i-- {
+		step := 1
+		for !stack.IsEmpty() && stack.Top().Int() < T[i] {
+			slice++
+			stack.Pop()
+		}
+		if stack.IsEmpty() {
+			step = 0
+		}
+		stack.Push(T[i])
+		ret[i] = step + slice
+	}
+	return ret
+}
