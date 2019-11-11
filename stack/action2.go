a260581726e340b28f1b4582e0312cb598d37484
mixichao <mixichao@jd.com>
2019-11-11 15:22:55 +0800

    [feat] 二叉树的中序遍历
    习题地址: https://leetcode-cn.com/problems/binary-tree-inorder-traversal/

--- stack/action2.go
+++ stack/action2.go
@@ -175,3 +175,41 @@ func calPoints(ops []string) int {
 	}
 	return score
 }
+
+/*
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	ret := inorderTraversal(root.Left)
+	if ret == nil {
+		ret = []int{root.Val}
+	} else {
+		ret = append(ret, root.Val)
+	}
+	LRet := inorderTraversal(root.Right)
+	if LRet != nil {
+		ret = append(ret, LRet...)
+	}
+	return ret
+}
