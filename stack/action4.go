51244944b1ad57fdc2cde00b81f216b6d55b310c
mixichao <mixichao@jd.com>
2019-11-12 12:03:08 +0800

    [feat] 二叉搜索树迭代器
    习题地址: https://leetcode-cn.com/problems/binary-search-tree-iterator/

--- stack/action4.go
+++ stack/action4.go
@@ -57,3 +57,70 @@ func evalRPN(tokens []string) int {
 	}
 	return stack.Pop().Int()
 }
+
+/*
+实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+调用 next() 将返回二叉搜索树中的下一个最小的数。
+
+示例：
+	7
+   / \
+  3  15
+    /  \
+   9   20
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // 返回 3
+iterator.next();    // 返回 7
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 9
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 15
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 20
+iterator.hasNext(); // 返回 false
+
+
+提示：
+next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+*/
+
+type BSTIterator struct {
+	stack Stack
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+	iter := BSTIterator{stack: Stack{}}
+	cur := root
+	for cur != nil {
+		iter.stack.Push(cur)
+		cur = cur.Left
+	}
+	return iter
+}
+
+/** @return the next smallest number */
+func (this *BSTIterator) Next() int {
+	node := this.stack.Pop()
+	val := node.val.(*TreeNode)
+	if val.Right != nil {
+		this.stack.Push(val.Right)
+	}
+	return val.Val
+}
+
+/** @return whether we have a next smallest number */
+func (this *BSTIterator) HasNext() bool {
+	if this.stack.node != nil {
+		return true
+	}
+	return false
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
