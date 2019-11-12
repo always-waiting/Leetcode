bcf5da31ab6a1f59544f8a288807711bfae8ae49
mixichao <mixichao@jd.com>
2019-11-12 14:08:03 +0800

    [feat] 删除字符串中的所有相邻重复项 II
    习题地址: https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string-ii/

--- stack/action4.go
+++ stack/action4.go
@@ -128,3 +128,67 @@ func (this *BSTIterator) HasNext() bool {
  * param_1 := obj.Next();
  * param_2 := obj.HasNext();
  */
+
+/*
+ 给你一个字符串 s，「k 倍重复项删除操作」将会从 s 中选择 k 个相邻且相等的字母，并删除它们，使被删去的字符串的左侧和右侧连在一起。
+你需要对 s 重复进行无限次这样的删除操作，直到无法继续为止。
+在执行完所有删除操作后，返回最终得到的字符串。
+本题答案保证唯一。
+
+示例 1：
+输入：s = "abcd", k = 2
+输出："abcd"
+解释：没有要删除的内容。
+
+示例 2：
+输入：s = "deeedbbcccbdaa", k = 3
+输出："aa"
+解释：
+先删除 "eee" 和 "ccc"，得到 "ddbbbdaa"
+再删除 "bbb"，得到 "dddaa"
+最后删除 "ddd"，得到 "aa"
+
+示例 3：
+输入：s = "pbbcggttciiippooaais", k = 2
+输出："ps"
+
+提示：
+1 <= s.length <= 10^5
+2 <= k <= 10^4
+s 中只含有小写英文字母。
+*/
+func removeDuplicates(s string, k int) string {
+	stackS := Stack{}
+	stackC := Stack{}
+	var count int
+	for _, val := range s {
+		if stackS.IsEmpty() {
+			stackS.Push(val)
+			continue
+		}
+		if stackS.Top().Rune() == val {
+			count++
+		} else {
+			stackC.Push(count)
+			count = 0
+		}
+		stackS.Push(val)
+		if count == k-1 {
+			for i := 0; i < k; i++ {
+				stackS.Pop()
+			}
+			tmp := stackC.Pop()
+			if tmp != nil {
+				count = tmp.Int()
+			} else {
+				count = 0
+			}
+		}
+	}
+	ret := ""
+	for !stackS.IsEmpty() {
+		val := stackS.Pop().Rune()
+		ret = string(val) + ret
+	}
+	return ret
+}
