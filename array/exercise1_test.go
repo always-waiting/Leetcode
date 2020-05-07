51441e6eef158317ca70b72245ac38117c7dd595
mixichao <mixichao@jd.com>
2020-05-07 17:59:25 +0800

    [Array] 移除元素

--- array/exercise1_test.go
+++ array/exercise1_test.go
@@ -15,4 +15,14 @@ func TestExercise1(t *testing.T) {
 			t.Log("检查正确")
 		}
 	}
+	{
+		t.Log("移除元素......")
+		a := []int{3, 2, 2, 3}
+		num := removeElement(a, 3)
+		if num != 2 {
+			t.Errorf("检查错误")
+		} else {
+			t.Log("检查正确")
+		}
+	}
 }
