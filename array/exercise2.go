fb2476be4ee2d2bb46719699909f650275b90e1f
mixichao <mixichao@jd.com>
2020-05-15 13:58:27 +0800

    [Array] 移动零

--- array/exercise2.go
+++ array/exercise2.go
@@ -9,6 +9,7 @@ Contents
 5. 存在重复元素[containsDuplicate]
 6. 存在重复元素II[containsNearbyDuplicate]
 7. 缺失数字[missingNumber]
+8. 移动零[moveZeroes]
 */
 
 /*
@@ -270,4 +271,44 @@ func missingNumber(nums []int) int {
 	}
 	exp := (n + 1) * n / 2
 	return exp - sum
+}
+
+/*
+移动零
+
+给定一个数组nums，编写一个函数将所有0移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+
+说明:
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+*/
+func moveZeroes(nums []int) {
+	/*
+		for i := 0; i < len(nums); i++ {
+			if nums[i] == 0 {
+				for j := i + 1; j < len(nums); j++ {
+					if nums[j] != 0 {
+						nums[i] = nums[j]
+						nums[j] = 0
+						break
+					}
+				}
+			}
+		}
+	*/
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[j] = nums[i]
+			j++
+		}
+	}
+	for j < len(nums) {
+		nums[j] = 0
+		j++
+	}
 }
