04b886527c938546f61f7bbaf056edee908773d6
mixichao <mixichao@jd.com>
2020-08-06 18:01:51 +0800

    [Iter] 回文对

--- daily/2020-08/example1.go
+++ daily/2020-08/example1.go
@@ -12,6 +12,7 @@ func main() {
 /*
 1. 字符串相加	--	https://leetcode-cn.com/problems/add-strings/
 2. 课程表	--	https://leetcode-cn.com/problems/course-schedule/
+3. 回文对	--	https://leetcode-cn.com/problems/palindrome-pairs/
 */
 
 /*
@@ -112,6 +113,119 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	}
 	for _, i := range degree {
 		if i != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+3. 回文对
+给定一组互不相同的单词，找出所有不同的索引对(i, j)，使得列表中的两个单词，words[i] + words[j] ，可拼接成回文串。
+
+示例 1：
+输入：["abcd","dcba","lls","s","sssll"]
+输出：[[0,1],[1,0],[3,2],[2,4]]
+解释：可拼接成的回文串为 ["dcbaabcd","abcddcba","slls","llssssll"]
+
+示例 2：
+输入：["bat","tab","cat"]
+输出：[[0,1],[1,0]]
+解释：可拼接成的回文串为 ["battab","tabbat"]
+*/
+/*
+// 超时!!
+func palindromePairs(words []string) [][]int {
+	ret := make([][]int, 0)
+	for i, word1 := range words {
+		for j := i + 1; j < len(words); j++ {
+			word2 := words[j]
+			if isPalindrome(word1 + word2) {
+				ret = append(ret, []int{i, j})
+			}
+			if isPalindrome(word2 + word1) {
+				ret = append(ret, []int{j, i})
+			}
+		}
+	}
+	return ret
+}
+
+func isPalindrome(in string) bool {
+	total := len(in)
+	mid := total / 2
+	for i := 0; i < mid; i++ {
+		if in[i] != in[total-1-i] {
+			return false
+		}
+	}
+	return true
+}
+*/
+type Node struct {
+	ch   [26]int
+	flag int
+}
+
+var tree []Node
+
+func palindromePairs(words []string) [][]int {
+	tree = []Node{Node{[26]int{}, -1}}
+	n := len(words)
+	for i := 0; i < n; i++ {
+		insert(words[i], i)
+	}
+	ret := [][]int{}
+	for i := 0; i < n; i++ {
+		word := words[i]
+		m := len(word)
+		for j := 0; j <= m; j++ {
+			if isPalindrome(word, j, m-1) {
+				leftId := findWord(word, 0, j-1)
+				if leftId != -1 && leftId != i {
+					ret = append(ret, []int{i, leftId})
+				}
+			}
+			if j != 0 && isPalindrome(word, 0, j-1) {
+				rightId := findWord(word, j, m-1)
+				if rightId != -1 && rightId != i {
+					ret = append(ret, []int{rightId, i})
+				}
+			}
+		}
+	}
+	return ret
+}
+
+func insert(s string, id int) {
+	add := 0
+	for i := 0; i < len(s); i++ {
+		x := int(s[i] - 'a')
+		if tree[add].ch[x] == 0 {
+			tree = append(tree, Node{[26]int{}, -1})
+			tree[add].ch[x] = len(tree) - 1
+		}
+		add = tree[add].ch[x]
+	}
+	tree[add].flag = id
+}
+
+// 找反向字符串是否在字典中!
+func findWord(s string, left, right int) int {
+	add := 0
+	for i := right; i >= left; i-- {
+		x := int(s[i] - 'a')
+		if tree[add].ch[x] == 0 {
+			return -1
+		}
+		add = tree[add].ch[x]
+	}
+	return tree[add].flag
+}
+
+func isPalindrome(s string, left, right int) bool {
+	for i := 0; i < (right-left+1)/2; i++ {
+		if s[left+i] != s[right-i] {
 			return false
 		}
 	}
--- daily/2020-08/example1_test.go
+++ daily/2020-08/example1_test.go
@@ -21,3 +21,30 @@ func Test_canFinish(t *testing.T) {
 		t.Errorf("课程表I结果错误: got(%v), expect(true)", ret)
 	}
 }
+
+func Test_isPalindrome(t *testing.T) {
+	words := []string{"abcd", "dcba", "lls", "s", "sssll"}
+	pairs := palindromePairs(words)
+	t.Log(pairs)
+	words = []string{"bat", "tab", "cat"}
+	pairs = palindromePairs(words)
+	t.Log(pairs)
+	words = []string{"a", ""}
+	pairs = palindromePairs(words)
+	t.Log(pairs)
+	words = []string{"a", "abc", "aba", ""}
+	pairs = palindromePairs(words)
+	t.Log(pairs)
+}
+
+func Test_f(t *testing.T) {
+	a := "abcd"
+	tree = []Node{Node{[26]int{}, -1}}
+	insert(a, 0)
+	insert("bcd", 1)
+	insert("aab", 2)
+	for _, node := range tree {
+		t.Log(node)
+	}
+	t.Log(findWord("dcba", 0, 3))
+}
