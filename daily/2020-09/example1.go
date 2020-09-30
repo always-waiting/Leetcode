66bc4fede5c04a9f3b5f6d3502cf5360646148a2
mixichao <mixichao@jd.com>
2020-09-30 10:39:00 +0800

    [Iter] 完成如下练习
    - 二叉搜索树中的插入操作

--- daily/2020-09/example1.go
+++ daily/2020-09/example1.go
@@ -23,6 +23,7 @@ func main() {
 7. 二叉搜索树的最近公共祖先		--	https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 8. 填充每个节点的下一个右侧节点指针 II		--	https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/
 9. 二叉树的后序遍历		--	https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+10. 701. 二叉搜索树中的插入操作		--	https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
 */
 
 /*
@@ -782,4 +783,56 @@ func postorderTraversal1(root *TreeNode) []int {
 		ret[i], ret[len(ret)-1-i] = ret[len(ret)-1-i], ret[i]
 	}
 	return ret
+}
+
+/*
+二叉搜索树中的插入操作
+给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据保证，新值和原始二叉搜索树中的任意节点值都不同。
+注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。
+例如,
+给定二叉搜索树:
+        4
+       / \
+      2   7
+     / \
+    1   3
+和 插入的值: 5
+你可以返回这个二叉搜索树:
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+或者这个树也是有效的:
+         5
+       /   \
+      2     7
+     / \
+    1   3
+         \
+          4
+提示：
+给定的树上的节点数介于 0 和 10^4 之间
+每个节点都有一个唯一整数值，取值范围从 0 到 10^8
+-10^8 <= val <= 10^8
+新值和原始二叉搜索树中的任意节点值都不同
+*/
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	if root.Val > val {
+		if root.Left == nil {
+			root.Left = &TreeNode{Val: val}
+		} else {
+			insertIntoBST(root.Left, val)
+		}
+	} else {
+		if root.Right == nil {
+			root.Right = &TreeNode{Val: val}
+		} else {
+			insertIntoBST(root.Right, val)
+		}
+	}
+	return root
 }
