244c53a59324f0f942c1b5b7e9bf19b41d2a7cb8
mixichao <mixichao@jd.com>
2020-11-04 10:50:08 +0800

    [Iter] 完成如下练习
    - [插入区间](https://leetcode-cn.com/problems/insert-interval/)

--- daily/2020-11/example1.go
+++ daily/2020-11/example1.go
@@ -10,6 +10,7 @@ func test() {
 
 /*
 1. 有效的山脉数组	--	https://leetcode-cn.com/problems/valid-mountain-array/
+2. 插入区间			--	https://leetcode-cn.com/problems/insert-interval/
 */
 
 /*
@@ -60,4 +61,73 @@ func validMountainArray(A []int) bool {
 		ret = false
 	}
 	return ret
+}
+
+/*
+57. 插入区间
+给出一个无重叠的，按照区间起始端点排序的区间列表。
+在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+
+示例 1：
+输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
+输出：[[1,5],[6,9]]
+示例 2：
+输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+输出：[[1,2],[3,10],[12,16]]
+解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
+*/
+func insert(intervals [][]int, newInterval []int) [][]int {
+	if len(intervals) == 0 {
+		return [][]int{newInterval}
+	}
+	ret := [][]int{}
+	cover := [][]int{newInterval}
+	left := [][]int{}
+	right := [][]int{}
+	for _, inter := range intervals {
+		flag := isCover(inter, newInterval)
+		switch flag {
+		case -1:
+			left = append(left, inter)
+		case 1:
+			right = append(right, inter)
+		case 0:
+			cover = append(cover, inter)
+		}
+	}
+	if len(left) != 0 {
+		ret = append(ret, left...)
+	}
+	if len(cover) != 0 {
+		mergeInter := merge(cover)
+		ret = append(ret, mergeInter)
+	}
+	if len(right) != 0 {
+		ret = append(ret, right...)
+	}
+	return ret
+}
+
+// 把重合的区间合并
+func merge(cover [][]int) []int {
+	min, max := cover[0][0], cover[0][1]
+	for _, val := range cover {
+		if min > val[0] {
+			min = val[0]
+		}
+		if max < val[1] {
+			max = val[1]
+		}
+	}
+	return []int{min, max}
+}
+
+// -1: a在b左; 0: a,b重叠; 1: a在b右
+func isCover(a []int, b []int) int {
+	if a[1] < b[0] {
+		return -1
+	} else if a[0] > b[1] {
+		return 1
+	}
+	return 0
 }
--- daily/2020-11/example1_test.go
+++ daily/2020-11/example1_test.go
@@ -7,3 +7,22 @@ import (
 func Test_validMountainArray(t *testing.T) {
 	t.Log(validMountainArray([]int{14, 82, 89, 84, 79, 70, 70, 68, 67, 66, 63, 60, 58, 54, 44, 43, 32, 28, 26, 25, 22, 15, 13, 12, 10, 8, 7, 5, 4, 3}))
 }
+
+func Test_insert(t *testing.T) {
+	t.Log(insert(
+		[][]int{[]int{1, 2}, []int{3, 5}, []int{6, 7}, []int{8, 10}, []int{12, 16}},
+		[]int{4, 8},
+	))
+	t.Log(insert(
+		[][]int{[]int{1, 2}, []int{5, 9}},
+		[]int{7, 8},
+	))
+	t.Log(insert(
+		[][]int{},
+		[]int{5, 6},
+	))
+	t.Log(insert(
+		[][]int{[]int{1, 5}},
+		[]int{6, 8},
+	))
+}
